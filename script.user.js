0320dff8ff2ede844887adbbd55e7c2c22c431bd
maikeg-alves <maicongabrielalves99@gmail.com>
2024-05-14 11:56:07 -0300

    :sparkles: feat: atualizaçoes por demanda

--- script.user.js
+++ script.user.js
@@ -8,8 +8,8 @@
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @run-at       document-start
 // @grant        none
-// @updateURL
-// @downloadURL
+// @updateURL    https://raw.githubusercontent.com/maikeg-alves/script_gestaoFila/main/script.meta.js
+// @downloadURL  https://github.com/maikeg-alves/script_gestaoFila/releases/latest/download/script.user.js
 // ==/UserScript==
 
 class Logger {
